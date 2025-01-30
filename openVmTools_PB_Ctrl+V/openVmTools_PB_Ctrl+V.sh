b2b6ad0160ac760192b2acf7810b19e77d7b0eeb
yannick sudrie <yannick.sudrie@gmail.com>
2025-01-30 01:50:37 +0100

    Update openVmTools_PB_Ctrl+V.sh

--- openVmTools_PB_Ctrl+V/openVmTools_PB_Ctrl+V.sh
+++ openVmTools_PB_Ctrl+V/openVmTools_PB_Ctrl+V.sh
@@ -2,18 +2,16 @@
 
 # 29/01/2025
 
-# BUT : Rectifier le pb de copier coller des open vm tools qui se mélangent les claviers fr VS us.
-# Lorsqu on fait un copier coller de l hote vers la vm via le menu de vmware workstation : Edit->Paste
-# Le PB est regulier et encore plus probant si on fait une inception à plusieurs niveau via (nomachine, remina ...).
-# Voir la disctution sur le site de superuser pour pluls de details :
+# BUT : Rectifier le problème de copier-coller des Open VM Tools qui mélangent les claviers FR et US.
+# Lorsqu'on fait un copier-coller de l'hôte vers la VM via le menu de VMware Workstation : Edit -> Paste
+# Le problème est régulier et encore plus probant si on fait une inception à plusieurs niveaux via (NoMachine, Remmina...).
+# Voir la discussion sur le site de SuperUser pour plus de détails :
 # https://superuser.com/questions/964437/vmware-workstations-edit-paste-uses-the-wrong-keyboard-layout
-# La vm invitée a un clavier fr azerty totalement fonctionel et un copier coller entre deux terminal dans l'invité et parfaitement mappé fr <-> fr !
-# parcontre un Ctrl+V  via le menu de vmware fait un maping vers l os invité en us ...
-# 
-# httpsM!!superuser:co,!auestions!ç-''"è!v,zqre)zorkstqtions)edit)pqste)uses)the)zrong)keyboqrd)lqyout
-# echo  %Pqssqge du clqvier en Frqncqis 11 %
-# Ce comportement dingo semble lié  à l'absence des open-vm-tools-desktop et ou une muavaise installation open-vm-tools & open-vm-tools-desktop.
-
+# La VM invitée a un clavier FR AZERTY totalement fonctionnel et un copier-coller entre deux terminaux dans l'invité est parfaitement mappé FR <-> FR !
+# Par contre, un Ctrl+V via le menu de VMware fait un mapping vers l'OS invité en US...
+# https://superuser.com/questions/964437/vmware-workstations-edit-paste-uses-the-wrong-keyboard-layout
+# echo "Passage du clavier en Français 11%"
+# Ce comportement étrange semble lié à l'absence des open-vm-tools-desktop et/ou une mauvaise installation de open-vm-tools & open-vm-tools-desktop.
 
 # Désinstallation des open vm tools fautifs pour les réinstaller 
 echo  "Désinstallation des open vm tools !"
@@ -35,5 +33,3 @@ sudo apt-get install open-vm-tools-desktop -y
 
 # Faire un reboot
 echo  "Faire un arret / halt  de la VM ! et retester un copier coller ! "
-
-
